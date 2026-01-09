9370632d6dcfacb16b86a45ea1f065eba5059cf8
Mansur Rizoev <mansur.rizoev.m@gmail.com>
2026-01-09 15:12:41 +0500

    Update cancellation rejection to return a specific error code in QR scanning for iOS

--- ios/Sources/BmQrPlugin/BmQrPlugin.swift
+++ ios/Sources/BmQrPlugin/BmQrPlugin.swift
@@ -48,7 +48,7 @@ extension BmQrPlugin: QRViewControllerDelegate {
             : language.lowercased() == "ru"
             ? "bmQr Activity было отменено"
             : "bmQr Activity was cancelled"
-        call?.reject('')
+        call?.reject('-1')
         qrViewController?.dismiss(animated: true)
         qrViewController = nil
         call = nil
