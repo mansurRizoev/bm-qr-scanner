f360841c556068e676bf940878f197e7bfdfe9e7
Mansur <mansur.rizoev.m@gmail.com>
2026-04-07 21:21:13 +0500

    scs

--- dist/plugin.cjs.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var core = require('@capacitor/core');
-
-const BmQrScanner = core.registerPlugin('BmQrPlugin');
-
-exports.BmQrScanner = BmQrScanner;
-//# sourceMappingURL=plugin.cjs.js.map
